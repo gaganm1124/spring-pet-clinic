963e35bae9efa036f5b10e75b02bdea868a4f09c
Gagan M <gagan@ncompass.in>
2022-04-17 13:09:02 +0530

    Added common Crud Interface. Closes #11

--- pet-clinic-data/src/main/java/gagan/springframework/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/gagan/springframework/springpetclinic/services/PetService.java
@@ -4,10 +4,6 @@ import gagan.springframework.springpetclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
+public interface PetService extends CrudService<Pet, Long>{
 
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
