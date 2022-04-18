77602243738b58551caaf1c6889cb88eeb8c5330
Gagan M <gagan@ncompass.in>
2022-04-18 15:00:18 +0530

    Added Specialities. Closes #27

--- pet-clinic-data/src/main/java/gagan/springframework/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/gagan/springframework/springpetclinic/model/Vet.java
@@ -1,11 +1,23 @@
 package gagan.springframework.springpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Speciality> specialities;
+
     public Vet() {
         super();
     }
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
     }
 }
